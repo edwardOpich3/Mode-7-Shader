f762b8ce8021315873351d3dbfe44461020944d0
Edward Opich <edwardOpich3@gmail.com>
2017-08-29 20:57:09 -0400

    Mouselook, vertical rotation, and more
    -Added mouselook, strafing is done with A and D, forward movement is W, backwards movement is S.

--- Main.cpp
+++ Main.cpp
@@ -10,8 +10,7 @@ int main()
 	bool quit = false;
 	bool redraw = false;
 	bool loading = true;
-	float FOV = 150.0f;
-	float FOVmult = tan((FOV * (ALLEGRO_PI / 180.0f)) / 2.0f);
+	float FOV = 32.0f;
 	float FPS = 60.0f;
 	int width = 1024;
 	int height = 896;
@@ -21,9 +20,9 @@ int main()
 
 	float angle = 0.0f;
 
-	float x = 924;
-	float y = 546;
-	float z = -32.0f;	// -16.0f for split screen!
+	float x = /*924*/ 0;
+	float y = /*546*/ 0;
+	float z = /*-32.0f*/ -1.0f;	// -16.0f for split screen!
 
 	float horizon = 32.0f; // From the top
 
@@ -57,18 +56,22 @@ int main()
 		return -1;
 	}
 
-	al_set_window_title(display, "Kart Racing Engine");
+	al_set_window_title(display, "Mode 7 Engine");
 
 	al_install_keyboard();
+	al_install_mouse();
 
 	al_init_image_addon();
 	//al_init_primitives_addon();
 
 	al_register_event_source(event, al_get_keyboard_event_source());
+	al_register_event_source(event, al_get_mouse_event_source());
 	al_register_event_source(event, al_get_timer_event_source(timer));
 	al_register_event_source(event, al_get_display_event_source(display));
 
 	al_start_timer(timer);
+
+	al_hide_mouse_cursor(display);
 
 	ALLEGRO_BITMAP* buffer = al_create_bitmap(256, 224);
 	ALLEGRO_BITMAP* mapBuffer = al_create_bitmap(256, 224);
@@ -82,6 +85,128 @@ int main()
 	//ALLEGRO_SHADER* defaultShader = NULL;
 	ALLEGRO_SHADER* mode7Shader = NULL;
 	ALLEGRO_SHADER* BGShader = NULL;
+
+	if (loading == true)
+	{
+		loading = false;
+
+		//al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
+		map = al_load_bitmap("data/map.png");
+		oob = al_load_bitmap("data/oob.png");
+		parallax1 = al_load_bitmap("data/parallax1.png");
+		parallax2 = al_load_bitmap("data/parallax2.png");
+
+		al_lock_bitmap(map, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+		al_lock_bitmap(oob, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+		al_lock_bitmap(parallax1, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+		al_lock_bitmap(parallax2, ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_READONLY);
+		//al_set_new_bitmap_flags(!ALLEGRO_MEMORY_BITMAP);
+
+		if (!(al_get_display_flags(display) & ALLEGRO_OPENGL))
+		{
+			mode7Shader = al_create_shader(ALLEGRO_SHADER_HLSL);
+			if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.hlsl"))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_PIXEL_SHADER, "Mode7 Pixel Shader.hlsl"))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_build_shader(mode7Shader))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+
+			BGShader = al_create_shader(ALLEGRO_SHADER_HLSL);
+			if (!al_attach_shader_source_file(BGShader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.hlsl"))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_attach_shader_source_file(BGShader, ALLEGRO_PIXEL_SHADER, "BG Pixel Shader.hlsl"))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_build_shader(BGShader))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+		}
+
+		else if (al_get_display_flags(display) & ALLEGRO_OPENGL)
+		{
+			mode7Shader = al_create_shader(ALLEGRO_SHADER_GLSL);
+			if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.glsl"))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_PIXEL_SHADER, "Mode7 Pixel Shader.glsl"))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_build_shader(mode7Shader))
+			{
+				std::cout << al_get_shader_log(mode7Shader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+
+			BGShader = al_create_shader(ALLEGRO_SHADER_GLSL);
+			if (!al_attach_shader_source_file(BGShader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.glsl"))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_attach_shader_source_file(BGShader, ALLEGRO_PIXEL_SHADER, "BG Pixel Shader.glsl"))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+			else if (!al_build_shader(BGShader))
+			{
+				std::cout << al_get_shader_log(BGShader);
+				std::cin.get();
+				std::cin.get();
+				quit = true;
+			}
+		}
+
+		//cout << al_get_default_shader_source(ALLEGRO_SHADER_GLSL, ALLEGRO_PIXEL_SHADER);
+		//cout << "Pixel format " << al_get_bitmap_format(map) << "\n";	// 9; ALLEGRO_PIXEL_FORMAT_ARGB_8888
+		if (!map)
+		{
+			return -1;
+		}
+	}
 
 	// Game Loop
 	while (!quit)
@@ -97,123 +222,6 @@ int main()
 		else if (e.type == ALLEGRO_EVENT_TIMER)
 		{
 			redraw = true;
-		}
-
-		if (loading == true)
-		{
-			loading = false;
-
-			//al_set_new_bitmap_flags(ALLEGRO_MEMORY_BITMAP);
-			map = al_load_bitmap("data/map.png");
-			oob = al_load_bitmap("data/oob.png");
-			parallax1 = al_load_bitmap("data/parallax1.png");
-			parallax2 = al_load_bitmap("data/parallax2.png");
-			//al_set_new_bitmap_flags(!ALLEGRO_MEMORY_BITMAP);
-
-			if (!(al_get_display_flags(display) & ALLEGRO_OPENGL))
-			{
-				mode7Shader = al_create_shader(ALLEGRO_SHADER_HLSL);
-				if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.hlsl"))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_PIXEL_SHADER, "Mode7 Pixel Shader.hlsl"))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_build_shader(mode7Shader))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-
-				BGShader = al_create_shader(ALLEGRO_SHADER_HLSL);
-				if (!al_attach_shader_source_file(BGShader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.hlsl"))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_attach_shader_source_file(BGShader, ALLEGRO_PIXEL_SHADER, "BG Pixel Shader.hlsl"))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_build_shader(BGShader))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-			}
-
-			else if (al_get_display_flags(display) & ALLEGRO_OPENGL)
-			{
-				mode7Shader = al_create_shader(ALLEGRO_SHADER_GLSL);
-				if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.glsl"))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_attach_shader_source_file(mode7Shader, ALLEGRO_PIXEL_SHADER, "Mode7 Pixel Shader.glsl"))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_build_shader(mode7Shader))
-				{
-					std::cout << al_get_shader_log(mode7Shader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-
-				BGShader = al_create_shader(ALLEGRO_SHADER_GLSL);
-				if (!al_attach_shader_source_file(BGShader, ALLEGRO_VERTEX_SHADER, "Vertex Shader.glsl"))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_attach_shader_source_file(BGShader, ALLEGRO_PIXEL_SHADER, "BG Pixel Shader.glsl"))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-				else if (!al_build_shader(BGShader))
-				{
-					std::cout << al_get_shader_log(BGShader);
-					std::cin.get();
-					std::cin.get();
-					quit = true;
-				}
-			}
-
-			//cout << al_get_default_shader_source(ALLEGRO_SHADER_GLSL, ALLEGRO_PIXEL_SHADER);
-			//cout << "Pixel format " << al_get_bitmap_format(map) << "\n";	// 9; ALLEGRO_PIXEL_FORMAT_ARGB_8888
-			if (!map)
-			{
-				return -1;
-			}
 		}
 
 		else if (e.type == ALLEGRO_EVENT_KEY_DOWN)
@@ -320,11 +328,38 @@ int main()
 			{
 				buttons[S] = false;
 			}
+
+			else if (e.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+			{
+				quit = true;
+			}
+		}
+
+		else if (e.type == ALLEGRO_EVENT_MOUSE_AXES)
+		{
+			angle += e.mouse.dx / 4.0f;
+
+			/*if (abs(FOV) > 1.0f)
+			{
+				FOV -= (e.mouse.dy / 4.0f) * (abs(FOV) / 32.0f);
+			}
+			else
+			{*/
+				FOV -= (e.mouse.dy / 4.0f);
+			//}
+			/*if (FOV  > 360.0f)
+			{
+			FOV = 360.0f;
+			}*/
+
+			al_set_mouse_xy(display, width / 2, height / 2);
 		}
 
 		// Update
 		if (redraw == true)
 		{
+			redraw = false;
+
 			double start = al_get_time();
 			if (buttons[LEFT])
 			{
@@ -338,13 +373,33 @@ int main()
 			}
 			if (buttons[UP])
 			{
-				y -= 2.0f * cos(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
-				x += 2.0f * sin(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
+				if (abs(FOV) > 1.0f)
+				{
+					FOV += 1.0f * (abs(FOV) / 32.0f);
+				}
+				else
+				{
+					FOV += 1.0f;
+				}
+				/*if (FOV  > 360.0f)
+				{
+				FOV = 360.0f;
+				}*/
 			}
 			if (buttons[DOWN])
 			{
-				y += 2.0f * cos(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
-				x -= 2.0f * sin(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
+				if (abs(FOV) > 1.0f)
+				{
+					FOV -= 1.0f * (abs(FOV) / 32.0f);
+				}
+				else
+				{
+					FOV -= 1.0f;
+				}
+				/*if (FOV < 0.0f)
+				{
+				FOV = 0.0f;
+				}*/
 			}
 			if (buttons[Z])
 			{
@@ -368,42 +423,36 @@ int main()
 				y += 2.0f * sin(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
 				x += 2.0f * cos(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
 			}
-			/*if (buttons[W])
+			if (buttons[W])
 			{
-				horizon += 1.0f;
-				if (horizon > 224.0f)
-				{
-					horizon = 224.0f;
-				}
+				y -= 2.0f * cos(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
+				x += 2.0f * sin(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
 			}
 			if (buttons[S])
 			{
-				horizon -= 1.0f;
-				if (horizon < 0.0f)
-				{
-					horizon = 0.0f;
-				}
-			}*/
+				y += 2.0f * cos(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
+				x -= 2.0f * sin(angle * (ALLEGRO_PI / 180.0f))/* * (-z / 16.0f)*/;
+			}
 
 			//FOVmult = tan((FOV * (ALLEGRO_PI / 180.0f)) / 2.0f);
 
 			ALLEGRO_TRANSFORM rotation;
 			al_identity_transform(&rotation);
-			al_translate_transform(&rotation, 0, 0);							// Move bitmap's origin to -x, -y
+			//al_translate_transform(&rotation, 128, 112);							// Move bitmap's origin to -x, -y
 
 			if (!(al_get_display_flags(display) & ALLEGRO_OPENGL))
 			{
-				al_scale_transform(&rotation, 1.0f, 2.0f);							// Scale it; 2.0f for single player, 4.0f for vertical split-screen
+				al_scale_transform(&rotation, 1.0f, /*64.0f / FOV*/ 1.0f);							// Scale it; 2.0f for single player, 4.0f for vertical split-screen
 			}
 
 			al_rotate_transform(&rotation, angle * (ALLEGRO_PI / 180.0f));		// Rotate it
 
 			if (al_get_display_flags(display) & ALLEGRO_OPENGL)
 			{
-				al_scale_transform(&rotation, 1.0f, 2.0f);
+				al_scale_transform(&rotation, 1.0f, /*horizon / 16.0f*/ 64.0f / horizon);
 			}
 
-			al_translate_transform(&rotation, 128, 112);						// Center the rotation point at 128, 112
+			//al_translate_transform(&rotation, x, 112);						// Center the rotation point at 128, 112
 
 			al_set_target_bitmap(mapBuffer);
 			al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -454,7 +503,7 @@ int main()
 				}
 			}
 
-			if (!al_set_shader_float("horizon", horizon))
+			if (!al_set_shader_float("horizon", FOV))
 			{
 				std::cout << "Failed to update horizon\n";
 			}
@@ -471,7 +520,7 @@ int main()
 				std::cout << "Failed to upload matrix\n";
 			}
 
-			al_draw_bitmap(mapBuffer, 0, horizon, NULL);
+			al_draw_bitmap(mapBuffer, 0, /*horizon*/ 0, NULL);
 			al_use_shader(BGShader);
 			al_identity_transform(&rotation);
 
@@ -483,17 +532,22 @@ int main()
 			{
 				std::cout << "Failed to update width 1\n";
 			}
-
-			al_draw_bitmap(parallax2, 0, 0, NULL);
+			
+			al_draw_bitmap(parallax2, 0, FOV - 31.0f, NULL);
 
 			if (!al_set_shader_float("width", 512.0f))
 			{
 				std::cout << "Failed to update width 2\n";
 			}
 
-			al_draw_bitmap(parallax1, 0, 0, NULL);
+			al_draw_bitmap(parallax1, 0, FOV - 31.0f, NULL);
 
-			al_use_shader(BGShader);
+			al_use_shader(NULL);
+
+			/*al_unlock_bitmap(map);
+			al_unlock_bitmap(oob);
+			al_unlock_bitmap(parallax1);
+			al_unlock_bitmap(parallax2);*/
 
 			//al_draw_bitmap(map, -x + 128, -y + 112, NULL);
 
--- Mode7 Pixel Shader.hlsl
+++ Mode7 Pixel Shader.hlsl
@@ -19,49 +19,65 @@ sampler2D o = sampler_state {
 
 float4 ps_main(VS_OUTPUT Input, float2 screenPos : VPOS) : COLOR0
 {
-   if (al_use_tex) {
-      // Everything here is in pixels!
-      Input.TexCoord.x = (Input.TexCoord.x * 256.0f);
-      Input.TexCoord.y = (Input.TexCoord.y * (224.0f - 32.0f));
+	if (al_use_tex) {
+		// Everything here is in pixels!
+		Input.TexCoord.x = (Input.TexCoord.x * 256.0f);
+		Input.TexCoord.y = (Input.TexCoord.y * 224.0f);
+		
+		// Center the view
+		Input.TexCoord.x -= 128.0f;
+		Input.TexCoord.y -= 224.0f;
+		
+		// Apply Transform
+		Input.TexCoord = mul(Input.TexCoord, transform);
 
-      // Center the view
-      Input.TexCoord.x -= 128.0f;
-      Input.TexCoord.y -= (112.0f - 16.0f);
+		// Mode 7!
+		if(horizon <= 0.0f)
+		{
+			Input.TexCoord.x = ((Input.TexCoord.x / ((screenPos.y - horizon /*32*/) / ((abs(horizon) / -gamePos.z) + 224.0f))));
+			Input.TexCoord.y = ((Input.TexCoord.y / ((screenPos.y - horizon /*32*/) / ((abs(horizon) / -gamePos.z) + 224.0f))));
+		}
+		else
+		{
+			Input.TexCoord.x = ((Input.TexCoord.x / ((screenPos.y - horizon /*32*/) / -((abs(horizon) / -gamePos.z) - 224.0f))));
+			Input.TexCoord.y = ((Input.TexCoord.y / ((screenPos.y - horizon /*32*/) / -((abs(horizon) / -gamePos.z) - 224.0f))));
+		}
+		//Input.TexCoord.y = ((Input.TexCoord.y / ((screenPos.y - horizon /*32*/) / -gamePos.z)));
 
-      // Apply Transform
-      Input.TexCoord = mul(Input.TexCoord, transform);
+		// Move view to x and y
+		Input.TexCoord.x += gamePos.x;
+		Input.TexCoord.y += gamePos.y;
 
-      // Mode 7!
-      Input.TexCoord.x = ((Input.TexCoord.x / ((screenPos.y - horizon) / -gamePos.z)));
-      Input.TexCoord.y = ((Input.TexCoord.y / ((screenPos.y - horizon) / -gamePos.z)));
-
-      // Move view to x and y
-      Input.TexCoord.x += gamePos.x;
-      Input.TexCoord.y += gamePos.y;
-
-      // If we're on the track, draw some track pixels...
-      if(Input.TexCoord.x >= 0.0f && Input.TexCoord.x < 1024.0f && Input.TexCoord.y >= 0.0f && Input.TexCoord.y < 1024.0f)
-      {
-         return Input.Color * tex2D(s, Input.TexCoord / 1024.0f);
-      }
-      // ... Otherwise, draw some of the out-of-bounds pixels.
-      else
-      {
-         Input.TexCoord = ((Input.TexCoord % 8) + 8) % 8;
-         Input.TexCoord *= 64.0f;
-         return Input.Color * tex2D(o, Input.TexCoord / 1024.0f);
-      }
-   }
-   else {
-      return Input.Color;
-   }
+		// If we're on the track, draw some track pixels...
+		if(screenPos.y >= horizon)
+			{
+			if(Input.TexCoord.x >= 0.0f && Input.TexCoord.x < 1024.0f && Input.TexCoord.y >= 0.0f && Input.TexCoord.y < 1024.0f)
+			{
+				return Input.Color * tex2D(s, Input.TexCoord / 1024.0f);
+			}
+			// ... Otherwise, draw some of the out-of-bounds pixels.
+			else
+			{
+				Input.TexCoord = ((Input.TexCoord % 8) + 8) % 8;
+				Input.TexCoord *= 64.0f;
+				return Input.Color * tex2D(o, Input.TexCoord / 1024.0f);
+			}
+		}
+		else
+		{
+			return float4((248.0f / 256.0f), (232.0f / 256.0f), (144.0f / 256.0f), 0);
+		}
+	}
+	else {
+		return Input.Color;
+	}
 }
 
 technique lol
 {
-  pass P0
-  {
-    PixelShader = compile ps_3_0 ps_main();
-    VertexShader = compile vs_3_0 vs_main();
-  }
+	pass P0
+	{
+		PixelShader = compile ps_3_0 ps_main();
+		VertexShader = compile vs_3_0 vs_main();
+	}
 }
